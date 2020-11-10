e622eb34dc04d6c4c03da04f9f1e862eefa62408
Pedro Castilho <pedro@castilho.in>
2020-11-10 07:30:17 -0300

    Move character

--- main.py
+++ main.py
@@ -15,3 +15,11 @@ kitchen.link_room(dining_hall, 'south')
 dining_hall.link_room(kitchen, 'north')
 dining_hall.link_room(ballroom, 'west')
 ballroom.link_room(dining_hall, 'east')
+
+current_room = kitchen
+
+while True:
+    current_room.get_details()
+    command = input("> ")
+    current_room = current_room.move(command)
+    print("\n")
